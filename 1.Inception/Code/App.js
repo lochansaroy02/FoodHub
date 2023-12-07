1271e04c88e60cb6f974605fba8ceacb5d8d9220
lochan.kumar.fit.csebct20@teamfuture.in <104918582+lochansaroy02@users.noreply.github.com>
2023-12-07 16:46:18 +0530

    ignite the app using parcel

--- 1.Inception/Code/App.js
+++ 1.Inception/Code/App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import * as ReactDOM from 'react-dom';
+
 let header = React.createElement("h1", {}, "Welcome to the internet ");
+let header2 = React.createElement("h1", {}, "Welcome to the internet ");
 let root = ReactDOM.createRoot(document.getElementById("root")); // assuming you have a div with id 'root' as the target container
 root.render(header);
