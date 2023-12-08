6f5da117097664473de126c19485c70fd6cf4a83
lochan.kumar.fit.csebct20@teamfuture.in <104918582+lochansaroy02@users.noreply.github.com>
2023-12-08 13:09:37 +0530

    3.Laying The foundation

--- 1.Inception/Code/App.js
+++ 1.Inception/Code/App.js
@@ -1,7 +1,39 @@
 import React from "react";
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from "react-dom";
 
-let header = React.createElement("h1", {}, "Welcome to the internet ");
-let header2 = React.createElement("h1", {}, "Welcome to the internet ");
+// React Element
+let header = (
+  <h1
+    id="head"
+    style={{
+      color: "white",
+      background: "green",
+      fontFamily: "cursive",
+      paddingLeft: "10px",
+    }}
+  >
+    This is heading created by jsx 🚀
+  </h1>
+);
+//React component ( arrow function which returns react element ())
+
+let Header = () => (
+  <div>
+    <Header2 />
+    <h1
+      id="head"
+      style={{
+        color: "white",
+        background: "green",
+        fontFamily: "cursive",
+        paddingLeft: "10px",
+      }}
+    >
+      This is heading created by jsx 🚀
+    </h1>
+  </div>
+);
+
 let root = ReactDOM.createRoot(document.getElementById("root")); // assuming you have a div with id 'root' as the target container
-root.render(header);
+root.render(<Header2 />);
+import React from "react";
