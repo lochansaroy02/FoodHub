8dc07906524f5f6b4581f2204b46d8e4e3b14df2
Lochan Kumar <104918582+lochansaroy02@users.noreply.github.com>
2024-02-03 15:42:55 +0530

    Update links.js

--- React/src/utils/links.js
+++ React/src/utils/links.js
@@ -1,13 +1,11 @@
 export const LOGO_URL = "https://cdn.dribbble.com/users/1635051/screenshots/4291569/socio_curry_logo-01.png"
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
 
-const cors = "https://cors-anywhere.herokuapp.com/"
 
 
-export const API_URL =  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4453039&lng=88.4056282&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const API_URL =  "https://api.allorigins.win/raw?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D15.8323892%26lng%3D78.0544946%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.369726076519406&lng=85.33414583862306&restaurantId=";
 
-// export const API_URL = "corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.369726076519406&lng=85.33414583862306&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-// export const MENU_API = "corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.369726076519406&lng=85.33414583862306&restaurantId=";
+
 
     
